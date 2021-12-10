8440a127227e434ec67cdc6ff7ca8dc76c276527
Dmitrii Limonov <disordervlz@gmail.com>
2021-12-10 18:55:55 +0300

    Add assets and prepare components

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { TeamComponent } from './team/team.component';
+import { FaqComponent } from './faq/faq.component';
+import { StoryComponent } from './story/story.component';
+import { AboutComponent } from './about/about.component';
+import { RoadmapComponent } from './roadmap/roadmap.component';
+import { MinterComponent } from './minter/minter.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    TeamComponent,
+    FaqComponent,
+    StoryComponent,
+    AboutComponent,
+    RoadmapComponent,
+    MinterComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule {
+}
---
+++ src/app/home/minter/minter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-minter',
+  templateUrl: './minter.component.html',
+  styleUrls: ['./minter.component.scss']
+})
+export class MinterComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
