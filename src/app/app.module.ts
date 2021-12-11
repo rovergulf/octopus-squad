a19722fbf16391b7cb8eb5ab0ab30965d841fdc9
Dmitrii Limonov <disordervlz@gmail.com>
2021-12-11 18:27:05 +0300

    add pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +17,13 @@ import { HomeModule } from "./home/home.module";
         BrowserModule,
         AppRoutingModule,
         SharedModule,
-        HomeModule
+        HomeModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
