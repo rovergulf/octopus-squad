44160ba108a55d15841edd32139c19bda11a99d8
cosmicintruder <littlegreenbagg@googlemail.com>
2021-12-13 20:55:57 +0300

    add responsive styles for header menu

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { filter, Subscription } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    public navBarActive: boolean = false;
+    private sub = new Subscription();
+
+    constructor(
+        private router: Router
+    ) {
     }
 
     ngOnInit(): void {
+        this.sub = this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe(() => {
+            this.navBarActive = false;
+        });
     }
 
+    ngOnDestroy(): void {
+        this.sub?.unsubscribe();
+    }
 }
