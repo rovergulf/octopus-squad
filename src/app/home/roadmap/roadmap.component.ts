2848824aae01fd996fcc194c82f3f5d31733ed15
Dmitrii Limonov <disordervlz@gmail.com>
2022-01-24 11:41:17 +0300

    Update roadmap

--- src/app/home/roadmap/roadmap.component.ts
+++ src/app/home/roadmap/roadmap.component.ts
@@ -11,43 +11,45 @@ export class RoadmapComponent implements OnInit {
         {
             image: '/assets/images/10.svg',
             points: [
+                '50% of profits go to the community wallet',
+                'Choosing a fund for donation by voting in discord',
                 'We are improving Discord and social media management',
-                'Drawing 10 OS among the community',
+                'Drawing 20 OS among the community',
             ]
         },
         {
             image: '/assets/images/25.svg',
             points: [
-                '3 ETH goes to the community wallet',
+                '50% of profits go to the community wallet',
                 'Choosing a fund for donation by voting in discord',
-                'Drawing 20 OS for nft holders and subscribers',
+                'Drawing 20 OS among the community',
             ]
         },
         {
             image: '/assets/images/50.svg',
             points: [
-                '5 ETH goes to the community wallet',
+                '50% of profits go to the community wallet',
                 'Voting for the fund for donations',
-                'Distribution 20 OS + 1ETH for nft holders and subscribers',
+                'Distribution 20 OS + 2ETH for nft holders and subscribers',
             ]
         },
         {
             image: '/assets/images/75.svg',
             points: [
-                '8 ETH goes to the community wallet',
+                '50% of profits go to the community wallet',
                 'Voting for the fund for donations',
                 'Development of exclusive OS merch',
-                'Exclusive Rare NFT giveaways',
+                'Drawing 20 OS among the community',
             ]
         },
         {
             image: '/assets/images/100.svg',
             points: [
-                '10 ETH goes to the community wallet',
-                'Voting for the fund for donations',
-                'Drawing 3 ETH for Rare NFT holders',
-                'Development of a new project.',
-                'Hooray we made this world cleaner!',
+                '50% of profits go to the community wallet',
+                '10% profit is raffled among holders  OS NFT',
+                '20% development of a new project',
+                `3 ETH will be raffled among OS NFT holders with unique features.
+                (There are only 3 such octopuses, items will be announced when passing 50% of roadmap)`,
             ]
         },
     ];
